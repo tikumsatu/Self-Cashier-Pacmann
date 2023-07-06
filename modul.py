149dbb88234f8763ed7c850324bd3c0fbc42a853
Mansur Sholeh <134694642+tikumsatu@users.noreply.github.com>
2023-07-06 15:49:27 +0700

    Update modul.py

--- modul.py
+++ modul.py
@@ -225,7 +225,69 @@ class Transaksi():
     print(tabulate(self.list_belanja, headers = self.list_belanja.keys()),'\n')
 
     print('*' * 55)
+    
+  def transaksi(self):
+    """
+    Sebuah method yang digunakan untuk menjalankan proses self-cashier
+    """
+    
+    id_transaksi = int(input('Masukan nomor ID anda: '))
+    message = f'Welcome to Super Cashire ID Pelanggan {id_transaksi}'
 
+    print('*' * len(message))
+    print(message)
+    print('*' * len(message))
+    """
+    Memasukkan ID transaksi dan menampilkan welcoming message
+    """
+
+    user_transaksi = Transaksi()
+    confirm1 = input('Apakah anda akan bertransaksi? (y/n) : ')
+    while confirm1 == "y":
+      print('\nKetik 1 untuk add item. \nKetik 2 untuk delete item. \nKetik 3 untuk edit item. \nKetik 4 untuk cek order. \nKetik 0 untuk membatalkan belanja')
+      print('*' * 35, '\n')
+      belanja = input('Masukkan pilihan anda: ')
+      if belanja == "1":
+        nama_item = input('Masukkan nama item: ')
+        jumlah_item = int(input('Masukkan jumlah item: '))
+        harga_item = int(input('Masukkan harga item: '))
+        user_transaksi.add_item(nama_item, jumlah_item, harga_item)
+        confirm1 = input('Apakah anda akan bertransaksi? (y/n) : ')
+
+      elif belanja == "2":
+        nama_item = input('Masukkan nama item yang akan dibatalkan: ')
+        user_transaksi.delete_item(nama_item)
+        confirm1 = input('Apakah anda akan bertransaksi? (y/n) : ')
+
+      elif belanja == "3":
+        print('\nKetik "nama" untuk edit nama item. \nKetik "jumlah" untuk edit jumlah item. \nKetik "harga" untuk edit harga item.')
+        print('*' * 35, '\n')
+        masukan = input("Masukkan pilihan anda: ").lower()
+        if masukan == "nama":
+          nama_item = input('Masukkan nama item yang akan diubah: ')
+          user_transaksi.update_item_name(nama_item)
+          confirm1 = input('Apakah anda akan bertransaksi? (y/n) : ')
+
+        elif masukan == "jumlah":
+          nama_item = input('Masukkan nama item yang akan diubah jumlahnya: ')
+          user_transaksi.update_item_qty(nama_item)
+          confirm1 = input('Apakah anda akan bertransaksi? (y/n) : ')
+
+        elif masukan == "harga":
+          nama_item = input('Masukkan nama item yang akan diubah harganya: ')
+          user_transaksi.update_item_price(nama_item)
+          confirm1 = input('Apakah anda akan bertransaksi? (y/n) : ')
+
+      elif belanja == "4":
+        user_transaksi.check_order()
+        confirm1 = input('Apakah anda akan bertransaksi? (y/n) : ')
+
+      elif belanja == "0":
+        user_transaksi.reset_transaction()
+        confirm1 = input('Apakah anda akan bertransaksi? (y/n) : ')
+
+    else:
+      user_transaksi.total_price()
     print('Total belanja anda : Rp ', sum(self.list_belanja['Total Harga']))
     print(diskon, 'total diskon belanja anda: Rp ', disc)
     print('Total tagihan belanja anda : Rp  ', pay)
